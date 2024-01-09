5eccbe2566a705884ac8c4b1233e65e4e42507cb
SerhiiMacInCloud <sv54@gcloud.ua.es>
2024-01-09 17:44:51 +0000

    empezando ej 2

---
+++ EntregaIOS/PruebaPropertyList/PruebaPropertyList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PruebaPropertyList
+//
+//  Created by user245981 on 1/9/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
