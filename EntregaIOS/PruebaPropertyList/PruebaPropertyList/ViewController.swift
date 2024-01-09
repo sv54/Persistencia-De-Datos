4d3626a141c6a7c3bbca2f3c51fa50005b69cfe6
SerhiiMacInCloud <sv54@gcloud.ua.es>
2024-01-09 18:00:18 +0000

    ej2 hecho

--- EntregaIOS/PruebaPropertyList/PruebaPropertyList/ViewController.swift
+++ EntregaIOS/PruebaPropertyList/PruebaPropertyList/ViewController.swift
@@ -8,12 +8,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    struct Alumno: Codable{
+        var nombre: String
+        var notas: [Float]
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let a1 = Alumno(nombre: "Pepe", notas: [5,8.5,10])
+        let a2 = Alumno(nombre: "Eva", notas: [10,9])
+        let alumnos = [a1,a2]
+        
+        var urlDocs = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0]
+        let urlPlist = urlDocs.appendingPathComponent("alumnos.plist")
+        let encoder = PropertyListEncoder()
+        
+        let data = try! encoder.encode(alumnos)
+        try! data.write(to: urlPlist)
     }
-
-
 }
-
